f00cc6181527c40f1b49afb60b2d174504d961af
Hugh Peterson <PetersonHughP@gmail.com>
2024-07-14 22:25:49 -0400

    edited the tests a bit

--- prototype/test_CFR.py
+++ prototype/test_CFR.py
@@ -1,8 +1,11 @@
+import json
 import os
 from poker_algorithms import KuhnPokerCFR
 
-with open("regrets.json", "r") as file:
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        "regrets.json"), "r") as file:
     json_string = file.read()
+    print(json.dumps(json_string))
     pokerbot = KuhnPokerCFR(json_string)
 
 # pokerbot.train(100000, 0.05, 1E3, 1E2)
